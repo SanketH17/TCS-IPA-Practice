9213eae26edf7e4a3c2ff4be2de27990d054cf57
Sanket Harvande <sanketharvande49@gmail.com>
2023-06-04 10:55:30 +0530

    added file

---
+++ Java/ProgrammingQ35Marks/CourseSolution.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+
+public class CourseSolution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Course[] courses = new Course[4];
+
+        for (int i = 0; i < courses.length; i++) {
+            int courseId = sc.nextInt();sc.nextLine();
+            String courseName = sc.nextLine();
+            String courseAdmin = sc.nextLine();
+            int quiz = sc.nextInt();sc.nextLine();
+            int handson = sc.nextInt();sc.nextLine();
+
+            courses[i] = new Course(courseId, courseName, courseAdmin, quiz, handson);
+        }
+        String admin = sc.nextLine();
+        int hand = sc.nextInt();
+
+        sc.close();
+
+        int ans1 = findAvgOfQuizByAdmin(courses, admin);
+        if (ans1 != 0) {
+            System.out.println(ans1);
+        } else {
+            System.out.println("No course found");
+        }
+
+        Course[] ans2 = sortCourseByHandsOn(courses, hand);
+
+        if (ans2 != null) {
+            for (int i = 0; i < ans2.length; i++) {
+                System.out.println(ans2[i].getcourseName());
+            }
+        } else {
+            System.out.println("No course found with mentioned attribute.");
+        }
+
+    }
+
+    public static int findAvgOfQuizByAdmin(Course[] courses, String admin) {
+        int sum = 0, count = 0;
+        for (int i = 0; i < courses.length; i++) {
+            if (courses[i].getcourseAdmin().equalsIgnoreCase(admin)) {
+                sum = sum + courses[i].getquiz();
+                count++;
+            }
+
+        }
+
+        if (count > 0) {
+            int avg = sum / count;
+            return avg;
+        } else {
+            return 0;
+        }
+    }
+
+    public static Course[] sortCourseByHandsOn(Course[] courses, int handson) {
+        int count = 0;
+        for (int i = 0; i < courses.length; i++) {
+            if (courses[i].gethandson() < handson) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            return null;
+        } else {
+            Course[] sortedCourses = new Course[count];
+            int index = 0;
+            for (Course course : courses) {
+                if (course.gethandson() < handson) {
+                    sortedCourses[index] = course;
+                    index++;
+                }
+            }
+
+            // perform sorting
+            for (int i = 0; i < count - 1; i++) {
+                for (int j = 0; j < count - i - 1; j++) {
+                    if (sortedCourses[j].gethandson() > sortedCourses[j + 1].gethandson()) {
+                        Course temp = sortedCourses[j];
+                        sortedCourses[j] = sortedCourses[j + 1];
+                        sortedCourses[j + 1] = temp;
+                    }
+                }
+            }
+
+            return sortedCourses;
+
+        }
+    }
+
+}
+
+class Course {
+    private int courseId;
+    private String courseName;
+    private String courseAdmin;
+    private int quiz;
+    private int handson;
+
+    public Course(int courseId, String courseName, String courseAdmin, int quiz, int handson) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseAdmin = courseAdmin;
+        this.quiz = quiz;
+        this.handson = handson;
+    }
+
+    // courseId
+    public int getcourseId() {
+        return this.courseId;
+    }
+
+    public void setcourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    // courseName
+    public String getcourseName() {
+        return this.courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    // courseAdmin
+    public String getcourseAdmin() {
+        return this.courseAdmin;
+    }
+
+    public void setcourseAdmin(String courseAdmin) {
+        this.courseAdmin = courseAdmin;
+    }
+
+    // quiz
+    public int getquiz() {
+        return this.quiz;
+    }
+
+    public void setquiz(int quiz) {
+        this.quiz = quiz;
+    }
+
+    // handson
+    public int gethandson() {
+        return this.handson;
+    }
+
+    public void sethandson(int handson) {
+        this.handson = handson;
+    }
+
+}
